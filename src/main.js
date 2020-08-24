962badb1a08a707da0744c5e4ec5c487d495033f
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-24 18:58:13 -0500

    assets

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import "./assets/css/main.styl";
+
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
