1f9534114a8d11a4be335c83c157c4e67f3408cf
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-24 20:46:57 -0500

    added libraries

--- src/main.js
+++ src/main.js
@@ -9,6 +9,26 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import "./assets/css/main.styl";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from "@fortawesome/free-solid-svg-icons";
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
