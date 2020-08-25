fb84e0660a57cfc85abcc90edcfbf0453ddd62d7
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-24 21:46:45 -0500

    lint

--- src/plugins/fontAwesome.js
+++ src/plugins/fontAwesome.js
@@ -2,34 +2,34 @@ import Vue from "vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-    faSkull,
-    faCrown,
-    faDungeon,
-    faHatWizard,
-    faHammer,
-    faGem
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem
 } from "@fortawesome/free-solid-svg-icons";
 import {
-    faVuejs,
-    faBootstrap,
-    faFontAwesome,
-    faGithub,
-    faBattleNet
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(
-    faSkull,
-    faCrown,
-    faDungeon,
-    faHatWizard,
-    faHammer,
-    faGem,
-    faVuejs,
-    faBootstrap,
-    faFontAwesome,
-    faGithub,
-    faBattleNet
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
 );
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
