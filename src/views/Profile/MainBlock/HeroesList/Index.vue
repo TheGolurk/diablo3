05d3802c644c80556b92fa71e7741a06cfde2649
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-26 14:24:40 -0500

    heroes list props and data

--- src/views/Profile/MainBlock/HeroesList/Index.vue
+++ src/views/Profile/MainBlock/HeroesList/Index.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
     <h1>Heroes List</h1>
+    <b-table dark :items="heroes" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "HeroesList"
+  name: "HeroesList",
+  props: {
+    heroes: {
+      required: true,
+      type: Array
+    }
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: "name",
+          label: "name"
+        },
+        {
+          key: "class",
+          label: "Class",
+          sortable: true
+        },
+        {
+          key: "kills",
+          label: "Elite Kills",
+          sortable: true
+        }
+      ]
+    };
+  }
 };
 </script>
