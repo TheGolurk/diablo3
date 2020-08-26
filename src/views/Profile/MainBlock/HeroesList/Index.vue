39455a73946a35f96bc7984cfcb2393ebb518996
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-26 15:39:01 -0500

    heroes list fixed

--- src/views/Profile/MainBlock/HeroesList/Index.vue
+++ src/views/Profile/MainBlock/HeroesList/Index.vue
@@ -1,19 +1,24 @@
 <template>
-  <div>
-    <h1>Heroes List</h1>
+  <div class="heroes-list border-top border-secondary mt-5 pt-5">
     <b-table
+      hover
+      striped
+      dark
       :items="heroes"
       :fields="fields"
-      dark
-      hover
-      small
-      striped
       stacked="sm"
-    />
+      small
+    >
+      <template v-slot:cell(name)="data">
+        <HeroIco :hero="data.item" />
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+import HeroIco from "./HeroIco";
+
 export default {
   name: "HeroesList",
   props: {
@@ -22,6 +27,7 @@ export default {
       type: Array
     }
   },
+  components: { HeroIco },
   data() {
     return {
       fields: [
